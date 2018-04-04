17d2a19d147a117ead3aae135b8d8b32f79d5798
Greg <greg@sportslock.com>
2018-04-03 23:28:48 -0500

    added modal component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,19 @@
 import React, {Component} from 'react';
 
+import NewAthleteModal from './NewAthleteModal';
+
 export default () => {
     return (
-<div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">
-      <h5 className="my-0 mr-md-auto font-weight-normal">SCA Athletics</h5>
-      <nav className="my-2 my-md-0 mr-md-3">
-        {/* <a className="p-2 text-dark" href="#">Athletes</a>
-        <a className="p-2 text-dark" href="#">Sports</a> */}
-        {/* <a className="p-2 text-dark" href="#">Support</a>
-        <a className="p-2 text-dark" href="#">Pricing</a> */}
-      </nav>
-      <a className="btn btn-outline-primary" href="#">Add New Athlete</a>
-    </div>
+      <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom">
+        <h5 className="my-0 mr-md-auto font-weight-normal">SCA Athletics</h5>
+        <nav className="my-2 my-md-0 mr-md-3">
+          {/* <a className="p-2 text-dark" href="#">Athletes</a>
+          <a className="p-2 text-dark" href="#">Sports</a> */}
+          {/* <a className="p-2 text-dark" href="#">Support</a>
+          <a className="p-2 text-dark" href="#">Pricing</a> */}
+        </nav>
+        <a className="btn btn-outline-primary" href="#" data-toggle="modal" data-target="#newAthleteModal">Add New Athlete</a>
+        <NewAthleteModal modalId="newAthleteModal" />
+      </div>
     );
 };
---
+++ src/components/NewAthleteForm.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+
+class NewAthleteForm extends React.Component {
+    render() {
+        return (
+            <form name="newAthleteForm">
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                    <label htmlFor="inputFirstName4">First Name</label>
+                    <input type="text" className="form-control" id="inputFistName4" placeholder="First Name" />
+                    </div>
+                    <div className="form-group col-md-6">
+                    <label htmlFor="inputLastName4">Last Name</label>
+                    <input type="text" className="form-control" id="inputLastName4" placeholder="Last Name" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="inputAddress">Address</label>
+                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="inputAddress2">Address 2</label>
+                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+                </div>
+                <div className="form-row">
+                    <div className="form-group col-md-6">
+                    <label htmlFor="inputCity">City</label>
+                    <input type="text" className="form-control" id="inputCity" />
+                    </div>
+                    <div className="form-group col-md-4">
+                    <label htmlFor="inputState">State</label>
+                    <select id="inputState" className="form-control" value="">
+                        <option>Choose...</option>
+                        <option>...</option>
+                    </select>
+                    </div>
+                    <div className="form-group col-md-2">
+                    <label htmlFor="inputZip">Zip</label>
+                    <input type="text" className="form-control" id="inputZip" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <div className="form-check">
+                    <input className="form-check-input" type="checkbox" id="gridCheck" />
+                    <label className="form-check-label" htmlFor="gridCheck">
+                        Check me out
+                    </label>
+                    </div>
+                </div>
+                <div className="form-row">
+                    <div className="col">
+                        <input type="text" className="form-control" placeholder="Grade" />
+                    </div>
+                    <div className="col">
+                        <select name="sport" className="form-control">
+                            <option>Football</option>
+                            <option>Cheerleading</option>
+                            <option>Girls Basketball</option>
+                            <option>Boys Basketball</option>
+                            <option>Girls Volleyball</option>
+                            <option>Boys Volleyball</option>
+                            <option>Soccer</option>
+                        </select>
+                    </div>
+                </div>
+
+            </form>
+        );
+    }
+}
+
+export default NewAthleteForm;
